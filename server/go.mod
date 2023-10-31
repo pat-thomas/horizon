6689c528bb410d20c5b555e58ba260a93e1f83dc
patthomassoftware <patthomassoftware@penguin>
2023-10-31 15:48:22 -0400

    simple web server to serve up json data prompt files on disk

--- server/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/patthomas/horizon
-
-go 1.21.3
-
-require github.com/gorilla/mux v1.8.0 // indirect
---
+++ server/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import datastore
+#from datastore import get_prompt, add_prompt, update_prompt
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask'
+
+@app.get('/user/<username>')
+def get_user_profile(username):
+    return f'{username}\'s profile'
+
+@app.get('/prompt/<prompt_id>')
+def get_prompt(prompt_id):
+    prompt_data = datastore.db_get_prompt(prompt_id)
+    if prompt_data: return prompt_data
+    return({
+        "message": "prompt not found"
+    }, 404)
+
+if __name__ == '__main__':
+    app.run()
